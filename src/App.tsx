2a76c3eb51f3253a7e81f8995221bb04165f03a9
bookun <kutsuzawa2851@gmail.com>
2020-09-22 13:44:39 +0900

    my react template

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,19 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { FunctionComponent } from "react";
+import { css } from "goober";
 
-function App() {
+const App: FunctionComponent = () => {
+  const red = css`
+    color: red;
+  `;
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
+        <p className={red}>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
-}
+};
 
 export default App;
